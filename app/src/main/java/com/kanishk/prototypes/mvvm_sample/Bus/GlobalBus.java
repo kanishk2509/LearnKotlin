546936680a1b92350fb7482d84cbb0f075f631c9
Kanishk <kanishk@Anit.local>
2017-06-03 02:45:12 +0530

    first commit

---
+++ app/src/main/java/com/kanishk/prototypes/mvvm_sample/Bus/GlobalBus.java
@@ -0,0 +1,8 @@
+package com.kanishk.prototypes.mvvm_sample.Bus;
+
+/**
+ * Created by kanishk on 31/05/17.
+ */
+
+public class GlobalBus {
+}
---
+++ app/src/main/java/com/kanishk/prototypes/mvvm_sample/ViewModel/MainActivityViewModel.java
@@ -0,0 +1,8 @@
+package com.kanishk.prototypes.mvvm_sample.ViewModel;
+
+/**
+ * Created by kanishk on 31/05/17.
+ */
+
+public class MainActivityViewModel {
+}
